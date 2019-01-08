c7faec8962159620e718469f8c074b76bf479d87
g770728y <g770728y@163.com>
2019-01-08 21:16:50 +0800

    Updates

---
+++ precache-manifest.e8d668ab762c45ef9b8275ac2952544b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "702f7da24ccbcb01b690",
+    "url": "/static/js/main.702f7da2.chunk.js"
+  },
+  {
+    "revision": "69a9e0113bf6bd7afb8a",
+    "url": "/static/js/1.69a9e011.chunk.js"
+  },
+  {
+    "revision": "702f7da24ccbcb01b690",
+    "url": "/static/css/main.f3c334b3.chunk.css"
+  },
+  {
+    "revision": "79d9be258690ea9e3dbbf3f474057fdd",
+    "url": "/index.html"
+  }
+];
